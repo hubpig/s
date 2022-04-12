05e39325ce17bfdc5e2de708375345d4136ee6b6
bin <bin@qq.com>
2022-04-12 21:50:00 +0800

    hotfix commit chongtuasdfds  hahahaa

--- mybatisplus/src/main/java/com/study/zcb/mybatisplus/Hello.java
+++ mybatisplus/src/main/java/com/study/zcb/mybatisplus/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
         System.out.println("meger");
         System.out.println("hot-fix 代码冲突");
         System.out.println("master 代码冲突");
+        System.out.println("push github");
     }
 }
