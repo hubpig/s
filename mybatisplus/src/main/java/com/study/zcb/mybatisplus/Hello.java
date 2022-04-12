c85870a973635c1644776977a9d8073cfb5b5755
bin <bin@qq.com>
2022-04-12 21:01:58 +0800

    commit second hahaha

--- mybatisplus/src/main/java/com/study/zcb/mybatisplus/Hello.java
+++ mybatisplus/src/main/java/com/study/zcb/mybatisplus/Hello.java
@@ -3,5 +3,6 @@ package com.study.zcb.mybatisplus;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hahaha");
+        System.out.println("hihihi");
     }
 }
